426835ed518076f62b87d597d7a13ca37c1df6e1
MacylaHood <2024570521@ufs4life.ac.za>
2025-03-12 19:17:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 st.title("Test run")
+variable=st.button("Click me")
+if variable:
+# Generate random time series data
+  time_series = np.random.randn(100)
+  fig, ax = plt.subplots()
+  ax.plot(time_series)
+  ax.set_title("Random 100 time series")
+  ax.set_xlabel("Units")
+  ax.set_ylabel("Time")
+  st.pyplot(fig)
 
-st.button("Click me")
+
